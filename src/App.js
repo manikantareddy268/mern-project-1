144f190889ebb87579bd2dcb61936b896a1c3288
Cheruku Manikanta Reddy <manikantareddy@Cherukus-MacBook-Air.local>
2025-06-18 15:26:11 +0530

    Create App Layout and include header & footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Home.js";
 import { Login } from "./Login.js";
+import AppLayout from "./layout/AppLayout.js";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<AppLayout><Home /></AppLayout>} />
+      <Route path="/login" element={<AppLayout><Login /></AppLayout>} />
     </Routes>
   );
 }
--- src/Login.js
+++ src/Login.js
@@ -2,11 +2,6 @@ function Login() {
     return(
         <div class="container-fluid text-center">
             <h1>Login</h1>
-            <div class="login-container">
-                <from>
-                    
-                </from>
-            </div>
         </div>
     );
 }
